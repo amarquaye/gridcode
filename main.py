c88950260f0e87e9a306dc355941de8942bcb7eb
Kobina <kobina.acquahkoomson@gmail.com>
2023-12-11 09:44:08 +0000

    Updated main file with newer features

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import csv
 import os
 import sys
 import logging
-from datetime import datetime 
+import hashlib
+import time
+from tabulate import tabulate
+from datetime import datetime
+from colorama import init, Fore, Style
 from csv2pdf import convert
 import pywriter as pw
 
+init(autoreset=True)
 
 class AssetManagementSystem:
 
@@ -45,6 +50,7 @@ class AssetManagementSystem:
             return 0
 
     def create_asset(self):
+        print(f"{Fore.GREEN}Creating Asset{Style.RESET_ALL}")
         last_id = self.get_last_asset_id()
         new_id = last_id + 1
 
@@ -52,8 +58,18 @@ class AssetManagementSystem:
 
         print("NOTE, all entries will be converted to upper case automatically\n")
 
-        sn = input("Enter asset serial  number(SN): ")
-        sn = sn.strip().upper()
+        if new_id > 1:
+            while True:
+                sn = input("Enter asset serial  number(SN): ")
+                sn = sn.strip().upper()
+
+                if self.is_serial_number_unique(sn):
+                    break
+                else:
+                    print(f"Serial number '{sn}' already exists. Please enter a unique serial number.\n")
+        else:
+            sn = input("Enter asset serial  number(SN): ")
+            sn = sn.strip().upper()
 
         asset_category = input("Enter asset category: ")
         asset_category = asset_category.strip().upper()
@@ -66,6 +82,9 @@ class AssetManagementSystem:
 
         assignee = input("Enter the name of the person you want to assign this asset: ")
         assignee = assignee.strip().upper()
+        if assignee == "":
+            assignee = "NOT ASSIGNED"
+
 
         description = input("Enter a short description about the asset: ")
         description = description.strip().upper()
@@ -97,7 +116,16 @@ class AssetManagementSystem:
         except PermissionError:
             print("Cannot access file\nPlease close your spreadsheet reader and try again!")
 
+    def is_serial_number_unique(self, serial_number):
+        with open('assets.csv', 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row[self.SN] == serial_number:
+                    return False
+        return True
+
     def read_assets(self):
+        print(f"{Fore.GREEN}Reading Assets{Style.RESET_ALL}")
         # List options
         print("\n1) Spread sheet")
         print("2) PDF")
@@ -105,33 +133,48 @@ class AssetManagementSystem:
 
         read_choice = input("Enter the choice of reading: ")
         read_choice = read_choice.strip().upper()
+        file_path = 'assets.csv'
 
         if read_choice == "1":
+            #Logging Activity
+            log_message = "Reading assets in spreadsheet."
+            self.log_activity(log_message)
             # Open in spreadsheet form
             os.startfile("assets.csv")
             sys.exit()
 
         elif read_choice == "2":
-            convert("assets.csv", "assets.pdf", orientation="L")
+            #Logging Activity
+            log_message = "Reading assets in PDF."
+            self.log_activity(log_message)
+
+            try:
+                convert("assets.csv", "assets.pdf", orientation="L")
+            except Exception as e:
+                print(f"Error {e}\nPlease terminate other instance of assest.pdf")
+                
             # Open in PDF format
             os.startfile("assets.pdf")
             sys.exit()
 
         elif read_choice == "3":
             try:
-                with open('assets.csv', newline='') as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    print("\nList of Assets:")
-                    for row in reader:
-                        print(
-                            f"ID: {row[self.ID]}, SN: {row[self.SN]}, CATEGORY: {row[self.CATEGORY]}, TYPE: {row[self.TYPE]}, "
-                            f"LOCATION: {row[self.LOCATION]}, ASSIGNEE: {row[self.ASSIGNEE]}, DESCRIPTION: {row[self.DESCRIPTION]}, COLOR: {row[self.COLOR]}, "
-                            f"STATUS: {row[self.STATUS]}")
+                with open(file_path, 'r') as csv_file:
+                    csv_reader = csv.reader(csv_file)
+        
+                    # Assuming the first row contains headers
+                    headers = next(csv_reader)
+        
+                    # Read data
+                    data = [row for row in csv_reader]
+        
+                    # Print tabulated data
+                    print(tabulate(data, headers=headers, tablefmt="fancy_grid"))
+
 
                     # Log the activity
                     log_message = "Read assets from the system."
                     self.log_activity(log_message)
-
                     print("\n")
             except FileNotFoundError:
                 print("No assets found.\n")
@@ -140,6 +183,7 @@ class AssetManagementSystem:
             print("Invalid choice")
 
     def search_assets(self):
+        print(f"{Fore.GREEN}Searching Assets{Style.RESET_ALL}")
         search_column = input("Enter the column to search in (ID / SN / CATEGORY / TYPE / LOCATION / ASSIGNEE / DESCRIPTION /COLOR / STATUS): ")
         search_column = search_column.strip().upper()
 
@@ -157,12 +201,18 @@ class AssetManagementSystem:
                         found_assets.append(row)
 
                 if found_assets:
+                    headers = [self.ID, self.SN, self.CATEGORY, self.TYPE, self.LOCATION, self.ASSIGNEE, self.DESCRIPTION, self.COLOR, self.STATUS]
                     print("\nFound Assets:")
-                    for found_asset in found_assets:
-                        print(
-                            f"ID: {found_asset[self.ID]}, SN: {found_asset[self.SN]}, CATEGORY: {found_asset[self.CATEGORY]}, TYPE: {found_asset[self.TYPE]}, "
-                            f"LOCATION: {found_asset[self.LOCATION]}, ASSIGNEE: {found_asset[self.ASSIGNEE]}, DESCRIPTION: {found_asset[self.DESCRIPTION]}, COLOR: {found_asset[self.COLOR]}, "
-                            f"STATUS: {found_asset[self.STATUS]}")
+                    data = [
+                        [found_asset[self.ID], found_asset[self.SN], found_asset[self.CATEGORY], found_asset[self.TYPE],
+                         found_asset[self.LOCATION], found_asset[self.ASSIGNEE], found_asset[self.DESCRIPTION],
+                         found_asset[self.COLOR], found_asset[self.STATUS]]
+                        for found_asset in found_assets
+                    ]
+                    print(tabulate(data, headers=headers, tablefmt="fancy_grid"))
+                    #Logging Activity
+                    log_message = f"Found assets with {search_column} = {search_value}"
+                    self.log_activity(log_message)
                 else:
                     print(f"No assets found with {search_column} = {search_value}.\n")
 
@@ -174,6 +224,7 @@ class AssetManagementSystem:
         
 
     def update_asset(self):
+        print(f"{Fore.GREEN}Updating Assets{Style.RESET_ALL}")
         sn = input("Enter the serial number of the asset you want to update: ")
         sn = sn.strip().upper()
 
@@ -217,6 +268,7 @@ class AssetManagementSystem:
             print("No assets found.\n")
 
     def delete_asset(self):
+        print(f"{Fore.GREEN}Deleting Assets{Style.RESET_ALL}")
         sn = input("Enter the serial number of the asset you want to delete: ")
         sn = sn.strip().upper()
 
@@ -251,19 +303,20 @@ class AssetManagementSystem:
         except FileNotFoundError:
             print("No assets found.\n")
 
-def log_in():
-    username_input = input("Enter your username: ")
-    password_input = input("Enter your password: ")
+init(autoreset=True)  # Initialize colorama
 
-    if user_manager.login(username_input, password_input):
-        print("Login successful!")
-        main()
-    else:
-        print("Login failed.")
+def print_yellow(text, rate=0.001):
+    YELLOW = '\033[93m'
+    RESET = '\033[0m'
+
+    for char in text:
+        sys.stdout.write(YELLOW + char + RESET)
+        sys.stdout.flush()
+        time.sleep(rate)
 
 class UserManager:
     def __init__(self):
-        self.log_file = 'activity_log.txt'
+        self.log_file = 'activity.log'
 
     def log_activity(self, message):
         timestamp = datetime.now().strftime("%A, %Y-%m-%d %I:%M:%S %p")
@@ -275,7 +328,7 @@ class UserManager:
             with open('user_data.csv', 'r', newline='') as csvfile:
                 csvreader = csv.DictReader(csvfile)
                 for row in csvreader:
-                    if row['username'] == username and row['password'] == password:
+                    if row['username'] == username and self.verify_password(row['password'], password):
                         log_message = f"'{username}' has logged in."
                         self.log_activity(log_message)
                         return True
@@ -289,12 +342,58 @@ class UserManager:
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
                 writer.writeheader()
 
+        hashed_password = self.hash_password(password)
+
         with open('user_data.csv', 'a', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
-            writer.writerow({'username': username, 'password': password})
+            writer.writerow({'username': username, 'password': hashed_password})
             log_message = f"Account created for user '{username}'."
             self.log_activity(log_message)
             return True
+
+    def hash_password(self, password):
+        # Use a secure hashing algorithm (e.g., SHA-256) for password hashing
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    def verify_password(self, stored_password, entered_password):
+        # Compare hashed passwords during login verification
+        return stored_password == self.hash_password(entered_password)
+
+def display_menu():
+    print("\n" + "=" * 30)
+    print(f"{Fore.CYAN}Welcome to Console Login{Style.RESET_ALL}")
+    print("=" * 30)
+    print("1. Login")
+    print("2. Create User")
+    print("3. Exit")
+
+def login(user_manager):
+    print("\n" + "=" * 30)
+    print(f"{Fore.YELLOW}Login{Style.RESET_ALL}")
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    # Dummy authentication (replace with your authentication logic)
+    if user_manager.login(username, password):
+        print(f"{Fore.GREEN}Login successful!{Style.RESET_ALL}")
+        return True
+    else:
+        print(f"{Fore.RED}Login failed. Invalid username or password.{Style.RESET_ALL}")
+        return False
+
+def create_user(user_manager):
+    print("\n" + "=" * 30)
+    print(f"{Fore.YELLOW}Create User{Style.RESET_ALL}")
+    new_username = input("Enter a new username: ")
+    new_password = input("Enter a new password: ")
+
+    # Dummy storage (replace with your user creation logic)
+    # Here, we are storing the username and a hash of the password.
+    # In a real application, use a secure storage mechanism.
+    if user_manager.create_account(new_username, new_password):
+        print(f"{Fore.GREEN}User created successfully!{Style.RESET_ALL}")
+    else:
+        print(f"{Fore.RED}Failed to create user.{Style.RESET_ALL}")
 
 
 # Main function
@@ -302,7 +401,9 @@ def main():
     asset_system = AssetManagementSystem()
 
     while True:
-        print("\nAsset Management System")
+        print("\n" + "=" * 30)
+        print(f"{Fore.CYAN}Asset Management System{Style.RESET_ALL}")
+        print("=" * 30)
         print("1. Create Asset")
         print("2. Read Assets")
         print("3. Update Asset")
@@ -310,7 +411,7 @@ def main():
         print("5. Search Asset")
         print("6) Exit")
 
-        choice = input("Enter your choice (1-5): ")
+        choice = input("Enter your choice (1-6): ")
         choice = choice.strip()
 
         if choice == '1':
@@ -327,21 +428,21 @@ def main():
 
             confirm = input("Are you sure you want to exit this application? [Y/N]: ")
             if confirm.strip().upper() == "Y":
-                print("Exiting the Asset Management System. Goodbye!\n")
-                print("AMS, powered by GridCode.")
-                sys.exit()
+                print(f"{Fore.CYAN}Exiting the Asset Management System. Goodbye!{Style.RESET_ALL}\n")
+                print(f"{Fore.YELLOW}AMS, powered by GridCode.{Style.RESET_ALL}")
+                break
             elif confirm.strip().upper() == "N":
                 main()
             else:
-                print(f"Invalid response, {confirm}\n")
+                print(f"{Fore.RED}Invalid response, {confirm}{Style.RESET_ALL}\n")
                 main()
         else:
-            print("Invalid choice. Please enter a number between 1 and 5.\n")
+            print(f"{Fore.RED}Invalid choice. Please enter a number between 1 and 6.{Style.RESET_ALL}\n")
 
 
 if __name__ == "__main__":
     
-    pw.write(rate=0.001, text=r""""
+    text_to_print=r""""
   ,----..           ,-.----.              ,---,            ,---,              ,----..             /   /   \              ,---,                ,---,. 
  /   /   \          \    /  \          ,`--.' |          .'  .' `\           /   /   \           /   .     :           .'  .' `\            ,'  .' | 
 |   :     :         ;   :    \         |   :  :        ,---.'     \         |   :     :         .   /   ;.  \        ,---.'     \         ,---.'   | 
@@ -355,28 +456,24 @@ if __name__ == "__main__":
 |   :    /          :   : :-'          ;   |.'         ;   :  .'            |   :    /            \   \ .'           ;   :  .'            |   :   .' 
  \   \ .'           |   |.'            '---'           |   ,.'               \   \ .'              `---`             |   ,.'              |   | ,'   
   `---`             `---'                              '---'                  `---`                                  '---'                `----'     
-""")
+"""
+    print_yellow(text_to_print, rate=0.001)
+
 
     user_manager = UserManager()
 
-    # Login
-    username_input = input("Enter your username: ")
-    password_input = input("Enter your password: ")
+    while True:
+        display_menu()
+        choice = input("Enter your choice (1-3): ")
 
-    if user_manager.login(username_input, password_input):
-        print("Login successful!")
-        main()
-    else:
-        print("Login failed.")
-        ans = input("Would you like to create an account?[Y/N]")
-        if ans.strip().upper() == "Y":
-            new_username = input("Enter a new username: ")
-            new_password = input("Enter a new password: ")
-            if user_manager.create_account(new_username, new_password):
-                print("Account created!") 
-                log_in()
-            else:
-                print("Failed to create an account.")
+        if choice == "1":
+            if login(user_manager):
+                # Continue to display menu after successful login
+                main()
+        elif choice == "2":
+            create_user(user_manager)
+        elif choice == "3":
+            print(f"{Fore.CYAN}Exiting. Goodbye!{Style.RESET_ALL}")
+            break
         else:
-            print("Alright! Thank you for coming.")
-            sys.exit()
+            print(f"{Fore.RED}Invalid choice. Please enter a valid option.{Style.RESET_ALL}")
